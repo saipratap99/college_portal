6c377b92f737607ca7a4f0af8ef7a6399e23639c
saipratap99 <122005067@sastra.ac.in>
2020-03-11 22:31:29 +0530

    replaced test/models files with given test files in test.zip

--- test/models/department_test.rb
+++ test/models/department_test.rb
@@ -1,7 +1,16 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
+# Tests for Department model
 class DepartmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Should define Department model' do
+    assert defined? Department
+  end
+
+  test 'Should create a new department with name' do
+    dept = Department.new name: 'EEE'
+    assert dept.save
+    assert dept.id
+  end
 end
